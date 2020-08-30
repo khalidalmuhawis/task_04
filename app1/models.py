ad61336005c276daaf0d94955b7f66c57ca29663
khalid almuhawis <khalidalmuhawis0@gmail.com>
2020-08-30 12:34:32 +0300

    create models (restaurant)

---
+++ app1/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    opening_time = models.TimeField()
+    opening_time = models.TimeField()
+
+    def __str__(self):
+        return self.name
